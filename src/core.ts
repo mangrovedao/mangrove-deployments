0922d5d54507cd0df3b06e03d0cf06b0f1e1492f
Espen Højsgaard <espen@mangrove.exchange>
2024-01-05 14:18:06 +0100

    ops: Add Sepolia deployment of mangrove-core v2.0.2 (#53)

--- src/core.ts
+++ src/core.ts
@@ -1,11 +1,17 @@
 import Mangrove_v2_0_1 from "./assets/core/v2.0.1/Mangrove.json";
 import MgvOracle_v2_0_1 from "./assets/core/v2.0.1/MgvOracle.json";
 import MgvReader_v2_0_1 from "./assets/core/v2.0.1/MgvReader.json";
+import Mangrove_v2_0_2 from "./assets/core/v2.0.2/Mangrove.json";
+import MgvOracle_v2_0_2 from "./assets/core/v2.0.2/MgvOracle.json";
+import MgvReader_v2_0_2 from "./assets/core/v2.0.2/MgvReader.json";
 import { DeploymentFilter, VersionDeployments } from "./types";
 import { findDeployment } from "./utils";
 
 // This is a sorted array (newest to oldest), exported for tests
-export const _mangroveDeployments: VersionDeployments[] = [Mangrove_v2_0_1];
+export const _mangroveDeployments: VersionDeployments[] = [
+  Mangrove_v2_0_2,
+  Mangrove_v2_0_1,
+];
 
 export const getMangroveVersionDeployments = (
   filter?: DeploymentFilter,
@@ -14,7 +20,10 @@ export const getMangroveVersionDeployments = (
 };
 
 // This is a sorted array (newest to oldest), exported for tests
-export const _mgvOracleDeployments: VersionDeployments[] = [MgvOracle_v2_0_1];
+export const _mgvOracleDeployments: VersionDeployments[] = [
+  MgvOracle_v2_0_2,
+  MgvOracle_v2_0_1,
+];
 
 export const getMgvOracleVersionDeployments = (
   filter?: DeploymentFilter,
@@ -23,7 +32,10 @@ export const getMgvOracleVersionDeployments = (
 };
 
 // This is a sorted array (newest to oldest), exported for tests
-export const _mgvReaderDeployments: VersionDeployments[] = [MgvReader_v2_0_1];
+export const _mgvReaderDeployments: VersionDeployments[] = [
+  MgvReader_v2_0_2,
+  MgvReader_v2_0_1,
+];
 
 export const getMgvReaderVersionDeployments = (
   filter?: DeploymentFilter,
