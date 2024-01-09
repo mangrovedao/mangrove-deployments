f6b0d6a4eb4f1c9d83cb475c5d42756fa3f245a4
Espen Højsgaard <espen@espen.dk>
2024-01-09 11:35:57 +0100

    fix: Consolidate core deployments in v2.0.1 asset files
    This is a workaround until a better querying mechanism is implemented which allows easy querying of the latest addresses for all networks.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # Next version
 
+-Consolidate core testnet deployments in the v2.0.1 assets files. This is a workaround until a better querying mechanism is implemented which allows easy querying of the latest addresses for all networks.
+
 # 1.0.2
 
 - Add Sepolia deployment of mangrove-core v2.0.2
--- src/core.ts
+++ src/core.ts
@@ -1,17 +1,11 @@
 import Mangrove_v2_0_1 from "./assets/core/v2.0.1/Mangrove.json";
 import MgvOracle_v2_0_1 from "./assets/core/v2.0.1/MgvOracle.json";
 import MgvReader_v2_0_1 from "./assets/core/v2.0.1/MgvReader.json";
-import Mangrove_v2_0_2 from "./assets/core/v2.0.2/Mangrove.json";
-import MgvOracle_v2_0_2 from "./assets/core/v2.0.2/MgvOracle.json";
-import MgvReader_v2_0_2 from "./assets/core/v2.0.2/MgvReader.json";
 import { DeploymentFilter, VersionDeployments } from "./types";
 import { findDeployment } from "./utils";
 
 // This is a sorted array (newest to oldest), exported for tests
-export const _mangroveDeployments: VersionDeployments[] = [
-  Mangrove_v2_0_2,
-  Mangrove_v2_0_1,
-];
+export const _mangroveDeployments: VersionDeployments[] = [Mangrove_v2_0_1];
 
 export const getMangroveVersionDeployments = (
   filter?: DeploymentFilter,
@@ -20,10 +14,7 @@ export const getMangroveVersionDeployments = (
 };
 
 // This is a sorted array (newest to oldest), exported for tests
-export const _mgvOracleDeployments: VersionDeployments[] = [
-  MgvOracle_v2_0_2,
-  MgvOracle_v2_0_1,
-];
+export const _mgvOracleDeployments: VersionDeployments[] = [MgvOracle_v2_0_1];
 
 export const getMgvOracleVersionDeployments = (
   filter?: DeploymentFilter,
@@ -32,10 +23,7 @@ export const getMgvOracleVersionDeployments = (
 };
 
 // This is a sorted array (newest to oldest), exported for tests
-export const _mgvReaderDeployments: VersionDeployments[] = [
-  MgvReader_v2_0_2,
-  MgvReader_v2_0_1,
-];
+export const _mgvReaderDeployments: VersionDeployments[] = [MgvReader_v2_0_1];
 
 export const getMgvReaderVersionDeployments = (
   filter?: DeploymentFilter,
